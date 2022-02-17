8f5c3bb5edffae9ceeb9d0ffa510b9e7a560f3f9
Naritaa <Naritaa@ZAKZRHB-CLLB24.ballito.local>
2022-02-17 11:54:44 +0200

    working on scanners and delimeters. Line scanner, two scanners. Added printwriters

--- src/main/java/Scanners/Scannerdelimeters.java
+++ src/main/java/Scanners/Scannerdelimeters.java
@@ -37,7 +37,7 @@ public class Scannerdelimeters {
                 String sportPos = lineSc.next();
                 
                 //processing
-                
+                //
                 System.out.println(firstName + " " + lName+ " " + age+ " " + sport + " " + sportPos );
                 
             }
