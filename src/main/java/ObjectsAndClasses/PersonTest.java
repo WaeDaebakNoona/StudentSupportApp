4677b33639a12fddc98c0bbfed5585632a919c61
Naritaa <Naritaa@ZAKZRHB-CLLB24.ballito.local>
2022-06-21 14:39:27 +0200

    array manipulation

--- src/main/java/ObjectsAndClasses/PersonTest.java
+++ src/main/java/ObjectsAndClasses/PersonTest.java
@@ -16,7 +16,7 @@ public class PersonTest {
     public static void main(String[] args) throws FileNotFoundException{
         
         PersonManager pm = new PersonManager();
-   
+   //dfh
         System.out.println(pm.toString());
     }
     
