920ff8a870eb5e1e3b59c842b860f84b67bdcc70
Naritaa <Naritaa@ZAKZRHB-CLLB24.ballito.local>
2022-06-22 08:48:01 +0200

    array manipulation again

--- src/main/java/ObjectsAndClasses/PersonUI.java
+++ src/main/java/ObjectsAndClasses/PersonUI.java
@@ -171,7 +171,7 @@ public class PersonUI extends javax.swing.JFrame {
     private void nameInputAncestorAdded(javax.swing.event.AncestorEvent evt) {//GEN-FIRST:event_nameInputAncestorAdded
         // TODO add your handling code here:
     }//GEN-LAST:event_nameInputAncestorAdded
-
+//githubbbb
     private void agesortButtActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_agesortButtActionPerformed
         try {
             // TODO add your handling code here:
